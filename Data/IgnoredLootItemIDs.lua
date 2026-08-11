ff5e12bac4a4aa7f9600d2fa1b08f56f85a32ba3
MattZeeX <mattzeex@gmail.com>
2026-08-11 04:35:48 -0500

    fix: exclude invalid (transmog) items from S2 loot generation

--- Data/IgnoredLootItemIDs.lua
+++ Data/IgnoredLootItemIDs.lua
@@ -2,4 +2,6 @@ local addonName, GreatVaultOddsNS = ...
 
 GreatVaultOddsNS.IgnoredLootItemIDs = {
     [268280] = true, -- Bonus loot only. Equippable transmog, but not eligible for the Great Vault.
+    [281227] = true, -- Bonus loot only. Equippable transmog, but not eligible for the Great Vault.
+    [275937] = true, -- Bonus loot only. Equippable transmog, but not eligible for the Great Vault.
 }
